49fb3532c603da62d39828fe0e45bef11c17dc6c
MAHIN-10x <mahin2069@gmail.com>
2026-08-15 14:08:38 +0600

    initial commit
    initial commit

---
+++ ReliefHub/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to ReliefHub</h1>
+      <p>Help when it matters most.</p>
+    </div>
+  );
+}
+
+export default Home;
