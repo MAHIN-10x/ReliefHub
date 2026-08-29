bf11d3531f446c337f9a8434771db5738aa5b220
Irinasadia <irina.sadia@gmail.com>
2026-08-29 22:45:42 +0600

    Loading Update

---
+++ ReliefHub/src/components/common/Loading.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Loading.css';
+
+/**
+ * Reusable Loading Spinner component
+ */
+const Loading = ({ message = 'Loading ReliefHub data...', fullScreen = false }) => {
+  if (fullScreen) {
+    return (
+      <div className="relief-loading-fullscreen">
+        <div className="relief-spinner" />
+        <p className="relief-loading-text">{message}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="relief-loading-inline">
+      <div className="relief-spinner" />
+      {message && <p className="relief-loading-text">{message}</p>}
+    </div>
+  );
+};
+
+export default Loading;
