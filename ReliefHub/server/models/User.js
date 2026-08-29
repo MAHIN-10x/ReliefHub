017b7b59873f0d35c8662225656daeb37f7bbeae
MAHIN-10x <mahin2069@gmail.com>
2026-08-29 12:36:58 +0600

    Update User.js

---
+++ ReliefHub/server/models/User.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, 'Please provide your full name'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide an email address'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ['Victim', 'Volunteer', 'NGO', 'Emergency Agency', 'Donor'],
+      default: 'Volunteer',
+    },
+    phone: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    organization: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    location: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hash password before saving if modified
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare password helper
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
