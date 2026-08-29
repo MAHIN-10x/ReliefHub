300892323b55df11525f6d6194b4d940112ece8b
Irinasadia <irina.sadia@gmail.com>
2026-08-29 22:43:02 +0600

    Added Button Feature

---
+++ ReliefHub/src/components/common/Button.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Button.css';
+
+/**
+ * Reusable Button Component for ReliefHub
+ * Supports variants: 'primary', 'danger', 'white', 'secondary', 'outline'
+ * Supports sizes: 'sm', 'md', 'lg'
+ */
+const Button = ({
+  children,
+  variant = 'primary',
+  size = 'md',
+  fullWidth = false,
+  disabled = false,
+  type = 'button',
+  onClick,
+  icon: Icon,
+  className = '',
+  ...props
+}) => {
+  const classes = [
+    'relief-btn',
+    `relief-btn--${variant}`,
+    `relief-btn--${size}`,
+    fullWidth ? 'relief-btn--full' : '',
+    disabled ? 'relief-btn--disabled' : '',
+    className,
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  return (
+    <button
+      type={type}
+      className={classes}
+      disabled={disabled}
+      onClick={onClick}
+      {...props}
+    >
+      {Icon && <span className="relief-btn-icon"><Icon size={size === 'sm' ? 16 : size === 'lg' ? 20 : 18} /></span>}
+      <span>{children}</span>
+    </button>
+  );
+};
+
+export default Button;
